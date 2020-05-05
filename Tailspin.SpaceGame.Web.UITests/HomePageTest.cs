537c9a9e5ada874fd4c0fda4d96e7071bf7a58dd
Anant Awadhut <anant.awadhut@hotmail.com>
2020-05-04 20:06:20 -0700

    Run Selenium UI tests , skip IE tests.

--- Tailspin.SpaceGame.Web.UITests/HomePageTest.cs
+++ Tailspin.SpaceGame.Web.UITests/HomePageTest.cs
@@ -13,7 +13,7 @@ namespace UITests
 {
     [TestFixture("Chrome")]
     [TestFixture("Firefox")]
-    [TestFixture("IE")]
+    //[TestFixture("IE")]
     public class HomePageTest
     {
         private string browser;
